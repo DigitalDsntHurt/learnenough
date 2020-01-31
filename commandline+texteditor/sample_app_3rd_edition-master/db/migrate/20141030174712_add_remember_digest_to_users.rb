54ad7173f30de7370d26918803bd7160df2ccaf3
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-01-31 09:06:44 -0800

    removed security vulnerabilities in app

--- commandline+texteditor/sample_app_3rd_edition-master/db/migrate/20141030174712_add_remember_digest_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddRememberDigestToUsers < ActiveRecord::Migration
-  def change   # function definition
-    add_column :users, :remember_digest, :string
-  end
-end
--- commandline+texteditor/sample_app_3rd_edition-master/db/migrate/20141111014949_add_picture_to_microposts.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToMicroposts < ActiveRecord::Migration
-  def change   # function definition
-    add_column :microposts, :picture, :string
-  end
-end
